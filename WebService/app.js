68de02db45d46f27502f48705a25ba465b4cc9fe
psi <psi@WS-PSI>
2018-07-27 11:36:00 +0200

    der auf einem SubFolder "WebService" entwickelte Simulator4AuthSvc ist in ein eigenes repository gewandert

--- WebService/app.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {
-        'content-type': 'text/plain'
-    });
-    res.end('Hello World');
-}).listen(1310);
